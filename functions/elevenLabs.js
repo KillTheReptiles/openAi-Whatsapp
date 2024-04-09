45e35238e6ee5d25014d3b1130b5a85f41c3e93a
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2024-04-09 09:37:17 +0200

    firebase functions implemented and elevenlabs working

--- functions/elevenLabs.js
+++ functions/elevenLabs.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const stream = require("stream");
 const util = require("util");
 const ffmpeg = require("fluent-ffmpeg");
+const os = require("os");
+const path = require("path");
+const { ElevenLabsClient } = require("elevenlabs");
 const { bucket } = require("./database/storage");
 require("dotenv").config();
 
@@ -14,65 +17,46 @@ console.log("TOKEN DE ELEVEN:", elevenLabsToken);
 // Convert fs.write into Promise version to handle async/await
 const pipeline = util.promisify(stream.pipeline);
 
-const textToSpeech = async (text) => {
+const textToSpeech = async (textAPI) => {
   try {
-    const voiceId = "21m00Tcm4TlvDq8ikWAM"; // Reemplaza 'tu-voice-id' con el ID de voz que desees usar
-    const url = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`;
-    const data = {
-      text: text,
-      model_id: "eleven_monolingual_v1",
-      voice_settings: {
-        stability: 0.5,
-        similarity_boost: 0.5,
-      },
-    };
-    const config = {
-      headers: {
-        Accept: "audio/mpeg",
-        "Content-Type": "application/json",
-        "xi-api-key": "22dc46cb2aad6535bd8e0cf212e27d90", // Usa tu propia clave de API de Eleven Labs
-      },
-      responseType: "stream", // Es importante configurar el tipo de respuesta como stream
-    };
+    const elevenlabs = new ElevenLabsClient({
+      apiKey: elevenLabsToken,
+    });
 
-    // Realiza la solicitud para obtener el audio
-    const response = await axios.post(url, data, config);
+    const audio = await elevenlabs.generate({
+      voice: "Rachel",
+      text: "" + textAPI,
+      model_id: "eleven_multilingual_v2",
+    });
 
     // Genera un nombre único para el archivo de audio
     const timestamp = new Date().getTime();
-    const audioFilePath = `./temp/voice_audio_${timestamp}.mp3`;
-    const oggFilePath = `./temp/voice_audio_${timestamp}.ogg`;
+    const audioFilePath = path.join(os.tmpdir(), `voice_audio_${timestamp}.mp3`);
+    const oggFilePath = path.join(os.tmpdir(), `voice_audio_${timestamp}.ogg`);
 
     // Guarda el audio en formato mp3
-    await pipeline(response.data, fs.createWriteStream(audioFilePath));
+    await pipeline(audio, fs.createWriteStream(audioFilePath));
     console.log(`Audio guardado en: ${audioFilePath}`);
 
     // Convierte el audio de mp3 a ogg
     await convertMp3ToOgg(audioFilePath, oggFilePath);
 
-    // Elimina el archivo mp3
-    fs.unlinkSync(audioFilePath);
-
-    // // Lee el archivo de audio en formato ogg
-    const audioData = fs.readFileSync(oggFilePath);
-
-    // Sube el archivo de audio a Firebase Storage
+    // Sube el archivo de audio a Firebase Storage sin guardar localmente
     console.log("Subiendo archivo de audio a Firebase Storage...");
     const fileName = `voice_audio_${timestamp}.ogg`;
     const fileUpload = bucket.file(fileName);
-    const stream = fileUpload.createWriteStream({
+    const storageStream = fileUpload.createWriteStream({
       metadata: {
         contentType: "audio/ogg",
       },
     });
-
-    await new Promise((resolve, reject) => {
-      stream.on("error", (err) => {
+    const urlPromise = await new Promise((resolve, reject) => {
+      storageStream.on("error", (err) => {
         console.error("Error al cargar el archivo en Firebase Storage:", err);
         reject(err);
       });
 
-      stream.on("finish", async () => {
+      storageStream.on("finish", async () => {
         console.log("Archivo de audio cargado en Firebase Storage con éxito.");
         // Obtiene la URL del archivo cargado
         const [url] = await fileUpload.getSignedUrl({ action: "read", expires: "01-01-2900" });
@@ -80,14 +64,17 @@ const textToSpeech = async (text) => {
         resolve(url);
       });
 
-      stream.end(audioData);
+      fs.createReadStream(oggFilePath).pipe(storageStream); // Piping the local file to Firebase Storage stream
     });
+    console.log("urlPromise:", urlPromise);
 
-    // Elimina el archivo ogg temporal
-    // fs.unlinkSync(oggFilePath);
+    // Elimina los archivos temporales
+    fs.unlinkSync(audioFilePath);
+    fs.unlinkSync(oggFilePath);
+    return urlPromise;
   } catch (error) {
-    console.error("Error en la función textToSpeech:", error);
-    // throw error; // Lanza el error para que pueda ser manejado adecuadamente por el llamador
+    console.error("Error en la función textToSpeech:");
+    throw error;
   }
 };
 
